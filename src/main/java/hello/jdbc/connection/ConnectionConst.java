d9fa60ab424c25cae14ec9dcbe97b82fca08c9e5
이효곤 <gon109m@gmail.com>
2023-04-30 15:30:43 +0900

    Initial commit

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc_test";
+    public static final String USERNAME="gongon";
+    public static final String PASSWORD="1234";
+
+}
