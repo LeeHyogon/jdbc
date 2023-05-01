5b06552cf4d4463226e0c1962f52e4b580b8b7cd
이효곤 <gon109m@gmail.com>
2023-05-01 13:21:25 +0900

    JDBC개발-조회

--- src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -1,15 +1,28 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 public class MemberRepositoryVOTest {
 
     MemberRepositoryVO repository= new MemberRepositoryVO();
 
     @Test
     public void curd() throws Exception {
-        Member member = new Member("memberV1", 10000);
+        //save
+        Member member = new Member("memberV4", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member=findMember {}", member == findMember);
+        log.info("member equals findMember {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
     }
 }
